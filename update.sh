dbcc601b110acb53a0595801afa63f589068ce6d
Mike Krüger <mikekrueger81@gmail.com>
2017-07-05 21:12:16 +0200

    add gnome-mpv package

--- update.sh
+++ update.sh
@@ -41,6 +41,7 @@ function cloneall() {
     git clone https://aur.archlinux.org/vibrancy-full-dark-blue.git
     git clone https://aur.archlinux.org/xed.git
     git clone https://aur.archlinux.org/xreader.git
+    git clone https://aur.archlinux.org/gnome-mpv.git
     
     #Get calamares package
     get_package "calamares" \
